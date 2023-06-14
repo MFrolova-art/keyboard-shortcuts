d12648e817f5f58d72628c4198dcac9369922e06
Margo <margo@Margos-MBP.home>
2023-06-14 10:23:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <main class="container has-background-light-grey mt-6">
+    <div :class="{ 'is-hidden': popUpActive }" class="is-flex is-flex-direction-column columns is-vcentered">
+      <h1 class="title has-text-centered has-text-info column is-vcentered">Keyboard Shortcuts</h1>
+    <button 
+      type="button" 
+      @click="togglePopUp"
+      class="button is-info is-large is-clickable is-hovered column is-vcentered"
+    >
+      Open Keyboard Shortcuts
+    </button>
+    </div>
+    <AppPopUp :popUpActive="popUpActive" @close="togglePopUp">
+      <div>
+        <h1 class="title has-text-centered has-text-info">List of Shortcuts</h1>
+        <ul class="has-text-centered">
+          <li v-for="(item, index) in items" class="mt-3">
+            <kbd>{{ item.firstKey }}</kbd> + <kbd>{{ item.secondKey }}</kbd>
+          </li>
+        </ul>
+      </div>
+    </AppPopUp>
+  </main>
+</template>
+
+<script>
+import AppPopUp from './components/AppPopUp.vue'
+import { ref } from 'vue'
+
+export default {
+  name: "App",
+  components: {
+    AppPopUp,
+  },
+  setup() {
+    const popUpActive = ref(false);
+
+    const togglePopUp = () => {
+      popUpActive.value = !popUpActive.value;
+    };
+
+    const items = ref([
+      { firstKey: 'Ctrl',
+        secondKey: 'A'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'C'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'E'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'F'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'L'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 's'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'P'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'D'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'Y'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'Shift'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'O'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'U'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'p'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'N'
+      },
+      { firstKey: 'Ctrl',
+        secondKey: 'I'
+      },
+     
+    ]);
+
+    return { popUpActive, togglePopUp, items };
+  },
+};
+</script>
+
---
+++ src/components/AppPopUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition name="popUp-animation">
+    <div v-show="popUpActive">
+      <div :class="{ 'has-background-white': popUpActive }" class="modal-content box">
+        <i @click="close" class="far fa-times-circle has-text-info is-flex is-justify-content-flex-end is-clickable"></i>
+        <!--PopUp Content-->
+        <slot />
+        <button 
+          type="button"
+          @click="close" 
+          class="button is-info is-light is-normal is-hovered is-clickable is-focused is-fullwidth mt-4 is-size-5"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ["popUpActive"],
+  setup(props, { emit }) {
+    const close = () => {
+      emit("close");
+    };
+
+    return { close };
+  },
+}
+</script>
+
+<style scoped>
+.popUp-animation-enter-active,
+.popUp-animation-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.popUp-animation-enter-from,
+.popUp-animation-leave-to {
+  opacity: 0;
+}
+</style>
